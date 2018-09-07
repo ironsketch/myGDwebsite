193fe6609bb61cbd380317397c3a79d47b3d33d2
ironsketch <null@nullface.net>
2018-09-07 12:27:36 -0700

    learning php

--- public_html/ltt/index.php
+++ public_html/ltt/index.php
@@ -23,30 +23,28 @@
 			</div>
 			<div id="cardHolder">
             <?php
+                function get_title($url){
+                    $str = file_get_contents($url);
+                    if(strlen($str)>0){
+                        $str = trim(preg_replace('/\s+/', ' ', $str));
+                        preg_match("/\<title\>(.*)\<\/title\>/i",$str, $title);
+                    return $title[1];
+                    }
+                }
                 $path = "docs/";
                     if ($handle = opendir($path)) {
                         while (false !== ($file = readdir($handle))) {
                             if ('.' === $file) continue;
                             if ('..' === $file) continue;
                             $html = explode(".", $file);
-                            if($html[1] === "html") echo $file;
+                            if($html[1] === "html"){
+                                $title = get_title("docs/" . $file);
+                                echo "<h1>" . $title . "</h1>";
+                            }
                         }
                     closedir($handle);
                     }
                 ?>          
-                <?php
-                    function get_title($url){
-                        $str = file_get_contents($url);
-                        if(strlen($str)>0){
-                            $str = trim(preg_replace('/\s+/', ' ', $str)); // supports line breaks inside <title>
-                            preg_match("/\<title\>(.*)\<\/title\>/i",$str,$title); // ignore case
-                            return $title[1];
-                
-                        }
-                    }
-                    $title = get_title("docs/gettingstarted.html");
-                    echo "<h1>" . $title . "</h1>";
-                ?>
                 <div id="card">
 					This is where a short description would go for what this document has.
 					<p class="start"><a href="" class="startLink">Start</a></p>
