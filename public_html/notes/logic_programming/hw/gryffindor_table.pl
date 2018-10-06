cf70cddc57b74d6e776bc251ced2760ec87f8683
ironsketch <null@nullface.net>
2018-10-05 21:25:12 -0700

    week1 logic

--- public_html/notes/logic_programming/hw/gryffindor_table.pl
+++ public_html/notes/logic_programming/hw/gryffindor_table.pl
@@ -1,4 +1,8 @@
-sits_right_of("Katie Bell","Parvati_Patil").
+/**
+* Michelle Bergin
+* Logic Programming
+*/
+sits_right_of("Katie Bell","Parvati Patil").
 sits_right_of("Natalie McDonald","Katie Bell").
 sits_right_of("Ron Weasley","Natalie McDonald").
 sits_right_of("Hermione Granger","Ron Weasley").
@@ -16,4 +20,9 @@ sits_right_of("Alicia Spinnet","Fred Weasley").
 sits_right_of("Neville Longbottom","Alicia Spinnet").
 sits_right_of("Lavender Brown","Neville Longbottom").
 sits_right_of("Parvati Patil","Lavender Brown").
-sits_right_of(X,Y).
+
+sits_left_of(X,Y):- sits_right_of(Y,X).
+are_neighbors_of(X,Y,Z):- sits_left_of(X,Z), sits_right_of(Y,Z).
+next_to_each_other(X,Y):- sits_right_of(X,Y).
+next_to_each_other(X,Y):- sits_left_of(X,Y).
+
