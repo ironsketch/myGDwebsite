3cd5b50f29dda9ae7cf8631fc38806ff3367f1b1
ironsketch <null@nullface.net>
2018-10-18 12:30:46 -0700

    fixing title not showing up from stuupeed php

--- public_html/notes/formal_language/index.php
+++ public_html/notes/formal_language/index.php
@@ -40,7 +40,7 @@
                             if ('..' === $file) continue;
                             $html = explode(".", $file);
                             if($html[1] === "html"){
-                                $title = get_title($path . $file);
+                                $title = get_title($file);
                                 echo "<div id='cardFormal'><span class='cardtitle'>" . $title . "</span><p class='start'><a href='" . $file . "' class='startLink'>Open</a></p></div>";
                             }
                         }
