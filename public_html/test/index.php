07c1df5064b771884a7ca40b16879b4157820ff4
ironsketch <null@nullface.net>
2018-10-22 14:32:57 -0700

    adding katex

--- public_html/test/index.php
+++ public_html/test/index.php
@@ -1,6 +1,9 @@
 <html>
     <head>
         <title>Testing an auto parser for creating webpage notes</title>
+        <link rel="stylesheet" href="katex.min.css">
+        <script src="smrender.js">
+        </script>
     </head>
     <body>
         <h1>My test page</h1>
@@ -11,6 +14,7 @@
             var ipbox = document.getElementById('pinput');
             ipbox.onkeyup = function(){
                 document.getElementById('text').innerHTML = ipbox.value;
+            katex.render("f(a,b,c) = (a^2+b^2+c^2)^3", text);
             }
         </script>
     </body>
