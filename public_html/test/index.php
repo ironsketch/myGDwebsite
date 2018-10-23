e5bc7f74bef054545961c0890ca5959c4c28eb64
ironsketch <null@nullface.net>
2018-10-23 11:51:47 -0700

    learning to use katex

--- public_html/test/index.php
+++ public_html/test/index.php
@@ -30,6 +30,7 @@
         <?php
             $thisLink = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
             $html = file_get_html($thisLink);
+            echo $html;
             $ret = $html->find('div[id=text]');
             echo $ret;
             $dom = new DOMDocument();
