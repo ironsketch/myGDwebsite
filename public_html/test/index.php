919411f5b573d7ea0e4d51541706fbd3e205a3ed
ironsketch <null@nullface.net>
2018-10-23 14:02:15 -0700

    learning to use katex

--- public_html/test/index.php
+++ public_html/test/index.php
@@ -14,31 +14,14 @@
             var ipbox = document.getElementById('pinput');
             ipbox.onkeyup = function(){
                 try{
-                    //var katexHTML = katex.renderToString(ipbox,{});
                     katex.render(ipbox.value, text);
                 }
-                catch(err){
-
+                    catch(err){
                 }
-            }
-            function myFunction(){
-                var textHTML = document.getElementById('text').innerHTML;
             }
         </script>
         <div id="demo">
         </div>
         <button onclick="myFunction()">Click me</button>
-        <?php
-            $thisLink = (isset($_SERVER['HTTPS']) && $_SERVE;
-            $html = file_get_html($thisLink);
-            echo $thisLink;
-            $ret = $html->find('div[id=text]');
-            echo $ret;
-            $dom = new DOMDocument();
-            $dom->loadHTML($html);
-            $xpath = new DOMXPath($dom);
-            $divContent = $xpath->query('//div[id="text"]');
-            
-        ?>
     </body>
 </html>
