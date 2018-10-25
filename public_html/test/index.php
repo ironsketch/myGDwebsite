abce8238c45bd303d96bc4f1362c79ee261bbb34
ironsketch <null@nullface.net>
2018-10-25 11:17:56 -0700

    fixing naming

--- public_html/test/index.php
+++ public_html/test/index.php
@@ -7,20 +7,13 @@
     </head>
     <body>
         <h1>My test page</h1>
-        <div id="text">
+        <div id="textArea">
         </div>
         <input type='text' id='pinput' onkeyup="butts()">
         <script>
-            var ipbox = document.getElementById('pinput');
-                function butts(){
+            function butts(){
                 try{
-                    var html = katex.renderToString("c = \\pm\\sqrt{a^2 + b^2}", {
-                        throwOnError: false
-
-                });
-                    //var html = katex.renderToString(ipbox, {});
-                    document.getElementById("demo").outerHTML = html;
-                    katex.render(ipbox.value, text);
+                    katex.render(ipbox.value, textArea);
                 }
                     catch(err){
                     }
@@ -29,7 +22,7 @@
         <?php
             function boobs(){
                 $title = "the date";
-                $body = $dom->getElementById('ipbox');
+                $body = $dom->getElementById('textArea');
                 //$body = 'test';
                 $myfile = fopen("butt/imaphppage.php", "w") or die ("unable to OPEN MOTHER FUCKER!");
                 $header = "<!DOCTYPE html>\n\t<head>\n\t\t<title>$title</title>\n\t</head>\n\t<body>\n\t\t$body\n\t</body>\n</html>\n";
