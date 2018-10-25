0977bcc7ed982c06a900645611cc46a4bb60f73f
ironsketch <null@nullface.net>
2018-10-25 09:08:48 -0700

    derfuq

--- public_html/test/index.php
+++ public_html/test/index.php
@@ -27,16 +27,21 @@
             }
         </script>
         <?php
-            $divs = $dom->getElementsByTagName('div');
-            
-            $title = "the date";
-            $body = "BUTTTTTTTTTTTTTTTTTs";
-            $myfile = fopen("butt/imaphppage.php", "w") or die ("unable to OPEN MOTHER FUCKER!");
-            $header = "<!DOCTYPE html><head><title>$title</title></head>\n<body>$body</body></html>";
-            fwrite($myfile,$header);
-            fclose($myfile);
+            function boobs(){
+                $divs = $dom->getElementsByTagName('div');
+                $title = "the date";
+                $body = "BUTTTTTTTTTTTTTTTTTs";
+                $myfile = fopen("butt/imaphppage.php", "w") or die ("unable to OPEN MOTHER FUCKER!");
+                $header = "<!DOCTYPE html><head><title>$title</title></head>\n<body>$body</body></html>";
+                fwrite($myfile,$header);
+                fclose($myfile);
+            }
+            if (isset($_GET['hello'])) {
+                boobs();
+            }
         ?>
         <div id="demo">
+            <a href='index.php?hello=true'>Run PHP Function</a>
         </div>
     </body>
 </html>
