526ca2228de06c299f216a4c3197411db6be7f2f
ironsketch <null@nullface.net>
2018-10-26 09:48:48 -0700

    getting js and php to work together

--- public_html/test/index.php
+++ public_html/test/index.php
@@ -9,8 +9,9 @@
         <h1>My test page</h1>
         <div id="textArea">
         </div>
-        <input type='text' id='pinput' onkeyup="butts()">
+        <input type='text' id='pinput'>
         <script>
+            var ipbox = document.getElementById('pinput');
             function butts(){
                 try{
                     katex.render(ipbox.value, textArea);
