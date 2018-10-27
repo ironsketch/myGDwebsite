a1e782d8180bcfae8d2b6770c5b218c3cae7e1d0
ironsketch <null@nullface.net>
2018-10-27 08:34:33 -0700

    getting js and php to work together

---
+++ public_html/test/2index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+    <head>
+        <title>Testing an auto parser for creating webpage notes</title>
+    </head>
+    <body>
+        <div id="textArea">
+        </div>
+        <?php
+            $dom = new domDocument;
+        ?>
+        <div id="demo">
+            <a href='index.php?hello=true'>Run PHP Function</a>
+        </div>
+    </body>
+</html>
