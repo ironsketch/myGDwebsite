79adf2a3748247c759a3e81bc89b8759ccbba3eb
ironsketch <null@nullface.net>
2018-10-27 06:41:39 -0700

    getting js and php to work together

--- public_html/test/index.php
+++ public_html/test/index.php
@@ -21,6 +21,7 @@
                     d.setTime(d.getTime() + (exdays*24*60*60*1000));
                     var expires = "expires="+ d.toUTCString();
                     document.cookie = "butt=boob";
+                    alert(document.cookie);
                     document.cookie = "katex=" + ipbox + ";" + expires + ";";
                 }
                     catch(err){
