7184646bb5dcf832b3d2d796e0cb725e85bbb2c4
ironsketch <null@nullface.net>
2018-10-30 04:27:39 -0700

    moving notes to folder n

--- public_html/notes/formal_language/index.php
+++ public_html/notes/formal_language/index.php
@@ -38,7 +38,7 @@
                         while (false !== ($file = readdir($handle))) {
                             if ('.' === $file) continue;
                             if ('..' === $file) continue;
-                            $html = explode(".", $file);
+                            $html = explode(".", $path.$file);
                             if($html[1] === "html" || $html[1] === "php"){
                                 $title = get_title($file);
                                 echo "<div id='cardFormal'><span class='cardtitle'>" . $title . "</span><p class='start'><a href='" . $file . "' class='startLink'>Open</a></p></div>";
