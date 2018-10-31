f32e58bf65db5f193512cfc74cf92ca50c737f1a
ironsketch <null@nullface.net>
2018-10-30 19:13:44 -0700

    fix

---
+++ public_html/notes/logic/n/10-30-18.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html><head><title>10-30-18</title><link rel="stylesheet" href="../styles.css"><link rel="stylesheet" href="../katex.min.css"><script src="../katex.js"></script></head><body><div id="container"><div id="top"><img style="float:right;padding-right:20px;width:250px;" src="../cat3.gif" /><div id="title"><h1>10-30-18</h1></div></div><div id="top_nav"><a class="top_nav" href="formal_language/">Formal</a><a class="top_nav" href="logic/">Logic</a><a class="top_nav" href="lambda/">Lambda</a><a class="top_nav" href="types/">Types</a></div><div id="cardHolder"><center><div id="textArea" style="padding-top:20px"></div><script>try{katex.render("\\textbf{3 Symantics}\\\\
+\\text{Operational}\\\\
+\\quad\\text{goal trees (or proof trees)}\\\\ 
+\\quad\\quad\\quad p(X\\text{ }X)\\\\
+\\quad\\quad/\\quad\\quad\\quad|\\\\
+\\quad q(X\\text{ }Y)\\quad r(X\\text{ }Z)\\\\
+\\text{Denotational}\\\\
+\\text{Axiomatic}\\\\
+\\quad\\text{(Horn)}\\\\
+\\text{ }\\\\
+\\quad5+3=3+5\\\\
+\\quad\\text{if }t_1\\text{ then }t_2\\text{ else }t_3\\\\
+\\quad\\text{What is the value?}\\\\
+\\quad\\text{You need to clarify to get value (remember the bar)}\\frac{something}{something}\\\\
+\\text{is\\_a\\_list([])}\\leftarrow\\text{A term is a list if it is an empty list.}\\\\
+\\text{}",textArea);} catch(err){}</script></center></div><div id="bottom"><br> Thanks for checking out my mafff and pergrehming sight.<p class="copyright">Copyright &copy; 2017 and beyond</p></div></div></body></html>
